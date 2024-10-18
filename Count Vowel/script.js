9154499ff1c50dbc53583f79b11e995eb22d6f38
Imran Khan <imran2013samirkhan@gmail.com>
2024-10-18 08:23:28 +0600

    Count vowel with Arrow function

---
+++ Count Vowel/script.js
@@ -0,0 +1,19 @@
+function countVowel(str) {
+    let count = 0;
+    for (const char of str) {
+        if (char === "a" || char === "e" || char === "i" || char === "o" || char === "u") {
+            count++;
+        }  
+    }
+    return count;
+}
+
+const countVow = (str) => {
+    let count = 0;
+    for (const char of str) {
+        if (char === "a" || char === "e" || char === "i" || char === "o" || char === "u") {
+            count++;
+        }  
+    }
+    return count;
+}

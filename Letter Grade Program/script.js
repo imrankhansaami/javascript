f426ce9017bc8dd9029d7dca39d61b2022580f37
Imran Khan <imran2013samirkhan@gmail.com>
2024-10-19 08:55:07 +0600

    Letter Grade Program

---
+++ Letter Grade Program/script.js
@@ -0,0 +1,16 @@
+// letter grade
+
+var marks = prompt("Enter your marks : ");
+
+if (marks >= 80)
+    console.log("A+");
+else if (marks >= 70)
+    console.log("A");
+else if (marks >= 60)   
+    console.log("B");
+else if (marks >= 50)
+    console.log("C");
+else if (marks >= 40)
+    console.log("D");
+else
+    console.log("F");

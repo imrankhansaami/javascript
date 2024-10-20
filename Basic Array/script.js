c6c40d879bfde3e744489693ab32a0f09087fc5c
Imran Khan <imran2013samirkhan@gmail.com>
2024-10-20 07:06:31 +0600

    Basic Array

---
+++ Basic Array/script.js
@@ -0,0 +1,23 @@
+var arnames = new Array();
+
+arnames[0] = "John";
+arnames[1] = "Cookie";
+arnames[2] = "Samir";
+
+console.log(arnames);
+
+var names = ["John", "Cookie", "Samir", "Usman", "Imran"];
+
+names.push("Khan");
+
+names.pop();
+
+console.log(names.length);
+console.log(names);
+
+var country1 = ["Bangladesh", "Pakistan", "India"];
+var country2 = ["Turkmenistan", "Turks"];
+
+var country = country1.concat(country2);
+
+console.log(country);
